17e003f15f5f2bda0e6c3f1e9887e17d5c9406e2
Ankit Kumar <ankit.kumar@truminds.com>
2021-06-01 11:17:42 +0530

    Initial project setup

---
+++ ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModularArchitecture_FeedLoader
+//
+//  Created by Ankit on 01/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
