261e7bbce95e752edb007be7179d05c7b1340439
Ankit Kumar <ankit.kumar@truminds.com>
2021-06-03 08:26:52 +0530

    Move RemoteFeedLoader Components to production

---
+++ ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoader/FeedApi/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  ModularArchitecture_FeedLoader
+//
+//  Created by Ankit on 03/06/21.
+//
+
+import Foundation
+
+
+public protocol HTTPClient {
+	func get(from url: URL)
+}
+
+
+public final class RemoteFeedLoader {
+	private let url: URL
+	private let client: HTTPClient
+	
+	public init(url: URL, client: HTTPClient) {
+		self.url = url
+		self.client = client
+	}
+	
+	public func load() {
+		client.get(from:url)
+	}
+}
