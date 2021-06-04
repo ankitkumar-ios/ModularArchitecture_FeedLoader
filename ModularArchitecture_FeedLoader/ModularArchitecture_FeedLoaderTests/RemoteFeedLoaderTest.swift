5f0e5ec79da8abed322e5b03bfd3ab7f882c956f
Ankit Kumar <ankit.kumar@truminds.com>
2021-06-04 08:59:06 +0530

    improve readability with helper method

--- ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoaderTests/RemoteFeedLoaderTest.swift
+++ ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoaderTests/RemoteFeedLoaderTest.swift
@@ -43,7 +43,7 @@ class RemoteFeedLoaderTest: XCTestCase {
 		sut.load {capturedErrors.append($0)}
 		
 		let clientError = NSError.init(domain: "Test", code: 0, userInfo: nil)
-		client.completions[0] (clientError)
+		client.complete(with: clientError)
 		
 		XCTAssertEqual(capturedErrors, [.connectivityError])
 	}
@@ -62,6 +62,10 @@ class RemoteFeedLoaderTest: XCTestCase {
 		func get(from url: URL, completion: @escaping (Error)-> Void){
 			completions.append(completion)
 			requestedURLs.append(url)
+		}
+		
+		func complete(with error: Error, at index: Int = 0) {
+			completions[index](error)
 		}
 
 	}
