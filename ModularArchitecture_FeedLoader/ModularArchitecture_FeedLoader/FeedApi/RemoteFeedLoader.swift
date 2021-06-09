89c2244c35b54e2377a29b787b29db5b1ba20173
Ankit Kumar <ankit.kumar@truminds.com>
2021-06-09 08:36:18 +0530

    delivers items array on 200HTTPResponse with json items

--- ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoader/FeedApi/RemoteFeedLoader.swift
+++ ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoader/FeedApi/RemoteFeedLoader.swift
@@ -40,10 +40,11 @@ public final class RemoteFeedLoader {
 		client.get(from:url) { result in
 			switch result {
 				case let .success(data, _):
-					if let _ = try? JSONSerialization.jsonObject(with: data){
-						completion(.success([]))
+					if let root = try? JSONDecoder().decode(Root.self, from: data){
+						completion(.success(root.items))
 					}
 					else {
+						
 						completion(.failure(.invalidData))
 					}
 				case .failure:
@@ -51,4 +52,9 @@ public final class RemoteFeedLoader {
 			}			
 		}
 	}
+}
+
+
+public struct Root: Decodable {
+	let items:[FeedItem]
 }
--- ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoaderTests/RemoteFeedLoaderTest.swift
+++ ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoaderTests/RemoteFeedLoaderTest.swift
@@ -75,6 +75,34 @@ class RemoteFeedLoaderTest: XCTestCase {
 			client.complete(withStatusCode: 200, data: emptyListData)
 		}
 	}
+	
+	func test_load_deliveryItemsOn200HTTPResponseWithJSONItem(){
+		let (sut, client) = makeSUT()
+		let item1 = FeedItem(id: UUID(), description: nil, location: nil, imageURL: URL.init(string: "https://a-url.com")!)
+		let item1JSON = [
+			"id": item1.id.uuidString,
+			"image": item1.imageURL.absoluteString,
+		]
+
+		let item2 = FeedItem(id: UUID(), description: "a description", location: "a location", imageURL: URL.init(string: "https://another-url.com")!)
+		
+		let item2JSON = [
+			"id": item2.id.uuidString,
+			"description": item2.description,
+			"location": item2.location,
+			"image": item2.imageURL.absoluteString,
+		]
+		
+		let itemsJSON = [
+			"items":[item1JSON, item2JSON]
+		]
+		
+		expect(sut, toCompleteWith: .success([item1, item2])) {
+			let json = try! JSONSerialization.data(withJSONObject: itemsJSON)
+			client.complete(withStatusCode: 200, data: json)
+		}
+		
+	}
 
 	
 	
