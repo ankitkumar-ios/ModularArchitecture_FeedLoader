9bfd1abe60e504bfb71c47e45fcdcbbc04a15c4f
Ankit Kumar <ankit.kumar@truminds.com>
2021-06-15 07:50:34 +0530

    gurantee correct url and http method of get request

--- ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoaderTests/FeedApi/URLSessionHTTPClientTest.swift
+++ ModularArchitecture_FeedLoader/ModularArchitecture_FeedLoaderTests/FeedApi/URLSessionHTTPClientTest.swift
@@ -29,6 +29,22 @@ class URLSessionHTTPClient{
 
 class URLSessionHTTPClientTest: XCTestCase {
 	
+	func test_getFromURL_performsGetRequestWithURL(){
+		URLProtocolStub.startInterceptingRequests()
+		let url = URL(string: "http://any-url.com")!
+		let exp = expectation(description: "Wait for request")
+		
+		URLProtocolStub.observerRequest{ request in
+			XCTAssertEqual(request.url, url)
+			XCTAssertEqual(request.httpMethod, "GET")
+			exp.fulfill()
+		}
+		
+		URLSessionHTTPClient().get(from: url) { _ in }
+		wait(for: [exp], timeout: 1.0)
+		URLProtocolStub.stopInterceptingRequests()
+	}
+	
 	func test_getFromURL_failsOnRequestError() {
 		
 		URLProtocolStub.startInterceptingRequests()
@@ -69,6 +85,8 @@ class URLSessionHTTPClientTest: XCTestCase {
 	private class URLProtocolStub: URLProtocol {
 		
 		private static var stub:Stubs?
+		private static var requestObserver:((URLRequest)-> Void)?
+		
 		private struct Stubs {
 			let data: Data?
 			let response: URLResponse?
@@ -78,6 +96,10 @@ class URLSessionHTTPClientTest: XCTestCase {
 			stub = Stubs.init(data:data, response: response, error: error)
 		}
 		
+		static func observerRequest(observer: @escaping (URLRequest)->Void) {
+			requestObserver = observer
+		}
+		
 		static func startInterceptingRequests() {
 			URLProtocol.registerClass(URLProtocolStub.self)
 		}
@@ -85,9 +107,11 @@ class URLSessionHTTPClientTest: XCTestCase {
 		static func stopInterceptingRequests() {
 			URLProtocol.unregisterClass(URLProtocolStub.self)
 			stub = nil
+			requestObserver = nil
 		}
 		
 		override class func canInit(with request: URLRequest) -> Bool {
+			requestObserver?(request)
 			return true
 		}
 		
